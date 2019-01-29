ac83a82b56309084d9ea561954fe8e14ea5fbeca
Olivier van den Biggelaar <ovdbigge@gmail.com>
2019-01-29 09:07:45 +0000

    Revert "Preventing replayability of failed tx to MultiSigWallet"
    This reverts commit 2f83b798d387ec54ee2a2aa1589efee9be6dc87c.

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -22,7 +22,7 @@ contract MultiSigWallet {
     event OwnerAdded(address indexed owner);
     event OwnerRemoved(address indexed owner);
     event ThresholdChanged(uint256 indexed newThreshold);
-    event Executed(bool indexed success, address indexed destination, uint256 indexed value, bytes data);
+    event Executed(address indexed destination, uint256 indexed value, bytes data);
     event Received(uint256 indexed value, address indexed from);
 
     /**
@@ -59,32 +59,31 @@ contract MultiSigWallet {
      * @param _signatures Concatenated signatures ordered based on increasing signer's address.
      */
     function execute(address _to, uint _value, bytes _data, bytes _signatures) public {
-        nonce += 1;
-
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
         uint256 count = _signatures.length / 65;
-        if(count < threshold) {
-            emit Executed(false, _to, _value, _data);
-            return;
-        }
-
-        bytes32 txHash = keccak256(abi.encodePacked(byte(0x19), byte(0), address(this), _to, _value, _data, nonce - 1));
+        require(count >= threshold, "MSW: Not enough signatures");
+        bytes32 txHash = keccak256(abi.encodePacked(byte(0x19), byte(0), address(this), _to, _value, _data, nonce));
+        nonce += 1;
         uint256 valid;
         address lastSigner = 0;
-        bool success;
         for(uint256 i = 0; i < count; i++) {
-            address recovered = recoverSigner(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", txHash)), _signatures, i);
+            (v,r,s) = splitSignature(_signatures, i);
+            address recovered = ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32",txHash)), v, r, s);
             require(recovered > lastSigner, "MSW: Badly ordered signatures"); // make sure signers are different
             lastSigner = recovered;
             if(isOwner[recovered]) {
                 valid += 1;
                 if(valid >= threshold) {
-                    // solium-disable-next-line security/no-call-value
-                    success = _to.call.value(_value)(_data);
-                    break;
+                    require(_to.call.value(_value)(_data), "MSW: External call failed");
+                    emit Executed(_to, _value, _data);
+                    return;
                 }
             }
         }
-        emit Executed(success, _to, _value, _data);
+        // If we reach that point then the transaction is not executed
+        revert("MSW: Not enough valid signatures");
     }
 
     /**
@@ -131,27 +130,22 @@ contract MultiSigWallet {
         emit Received(msg.value, msg.sender);        
     }
 
-    /**
-    * @dev Recovers the signer at a given position from a list of concatenated signatures.
-    * @param _signedHash The signed hash
-    * @param _signatures The concatenated signatures.
-    * @param _index The index of the signature to recover.
-    */
-    function recoverSigner(bytes32 _signedHash, bytes _signatures, uint _index) internal pure returns (address) {
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
+        /**
+     * @dev Parses the signatures and extract (r, s, v) for a signature at a given index.
+     * A signature is {bytes32 r}{bytes32 s}{uint8 v} in compact form and signatures are concatenated.
+     * @param _signatures concatenated signatures
+     * @param _index which signature to read (0, 1, 2, ...)
+     */
+    function splitSignature(bytes _signatures, uint256 _index) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
         // we jump 32 (0x20) as the first slot of bytes contains the length
         // we jump 65 (0x41) per signature
-        // for v we load 32 bytes ending with v (the first 31 come from s) then apply a mask
-        // solium-disable-next-line security/no-inline-assembly
+        // for v we load 32 bytes ending with v (the first 31 come from s) tehn apply a mask
         assembly {
             r := mload(add(_signatures, add(0x20,mul(0x41,_index))))
             s := mload(add(_signatures, add(0x40,mul(0x41,_index))))
             v := and(mload(add(_signatures, add(0x41,mul(0x41,_index)))), 0xff)
         }
         require(v == 27 || v == 28, "MSW: Invalid v"); 
-        return ecrecover(_signedHash, v, r, s);
     }
 
 }
