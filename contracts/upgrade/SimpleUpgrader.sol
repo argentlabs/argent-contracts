8b4b491806873bd6eac57944bfa9391060d37a07
Olivier van den Biggelaar <ovdbigge@gmail.com>
2019-10-24 12:38:24 +0100

    Fixing SimpleUpgrader

--- contracts/upgrade/SimpleUpgrader.sol
+++ contracts/upgrade/SimpleUpgrader.sol
@@ -37,11 +37,11 @@ contract SimpleUpgrader is BaseModule {
     function init(BaseWallet _wallet) public onlyWallet(_wallet) {
         uint256 i;
         //add new modules
-        for(i = 0; i < toEnable.length; i++) {
+        for(; i < toEnable.length; i++) {
             BaseWallet(_wallet).authoriseModule(toEnable[i], true);
         }
         //remove old modules
-        for(; i < toDisable.length; i++) {
+        for(i = 0; i < toDisable.length; i++) {
             BaseWallet(_wallet).authoriseModule(toDisable[i], false);
         }
         // SimpleUpgrader did its job, we no longer need it as a module
