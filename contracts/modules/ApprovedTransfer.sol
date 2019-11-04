ac069105db2af5fdfdb9191361d62a3c08d702a1
Julien Niset <julien.niset@gmail.com>
2019-11-04 18:29:53 +0100

    fix error string

--- contracts/modules/ApprovedTransfer.sol
+++ contracts/modules/ApprovedTransfer.sol
@@ -73,7 +73,7 @@ contract ApprovedTransfer is BaseModule, RelayerModule, BaseTransfer {
         onlyExecute
         onlyWhenUnlocked(_wallet)
     {
-        require(!_wallet.authorised(_contract) && _contract != address(_wallet), "TM: Forbidden contract");
+        require(!_wallet.authorised(_contract) && _contract != address(_wallet), "AT: Forbidden contract");
         doCallContract(_wallet, _contract, _value, _data);
     }
 
