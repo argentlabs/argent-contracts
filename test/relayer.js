8c50bad186f81492d8170c60ea9124c4171ade25
Olivier van den Biggelaar <ovdbigge@gmail.com>
2020-03-27 12:09:40 +0000

    Adding tests for RelayerModule and RelayerModuleV2

---
+++ test/relayer.js
@@ -0,0 +1,92 @@
+const { formatBytes32String } = require("ethers").utils;
+
+const Wallet = require("../build/BaseWallet");
+const TestModule = require("../build/TestModule");
+const TestModuleRelayerV2 = require("../build/TestModuleRelayerV2");
+const Registry = require("../build/ModuleRegistry");
+
+const TestManager = require("../utils/test-manager");
+const { getRandomAddress } = require("../utils/utilities.js");
+
+
+const TARGET_OF_DATA_NOT_WALLET_REVERT_MSG = "RM: the wallet authorized is different then the target of the relayed data";
+const TARGET_OF_DATA_NOT_WALLET_REVERT_MSG_V2 = "RM: Target of _data != _wallet";
+const INVALID_DATA_REVERT_MSG = "RM: Invalid dataWallet";
+const DUPLICATE_REQUEST_REVERT_MSG = "RM: Duplicate request";
+
+describe("Test RelayerModule", function () {
+  this.timeout(10000);
+
+  const manager = new TestManager();
+  const { deployer } = manager;
+  let { getNonceForRelay } = manager;
+  getNonceForRelay = getNonceForRelay.bind(manager);
+  const owner = global.accounts[1].signer;
+  let wallet;
+  let relayerModule;
+  let relayerModuleV2;
+
+  beforeEach(async () => {
+    const registry = await deployer.deploy(Registry);
+    relayerModule = await deployer.deploy(TestModule, {}, registry.contractAddress, false, 0);
+    relayerModuleV2 = await deployer.deploy(TestModuleRelayerV2, {}, registry.contractAddress, false, 0);
+    await registry.registerModule(relayerModule.contractAddress, formatBytes32String("RelayerModule"));
+    await registry.registerModule(relayerModuleV2.contractAddress, formatBytes32String("RelayerModuleV2"));
+    wallet = await deployer.deploy(Wallet);
+    await wallet.init(owner.address, [relayerModule.contractAddress, relayerModuleV2.contractAddress]);
+  });
+
+  describe("RelayerModule", () => {
+    it("should fail to relay when target of _data != _wallet", async () => {
+      const params = [await getRandomAddress(), 4]; // the first argument is not the wallet address, which should make the relaying revert
+      await assert.revertWith(
+        manager.relay(relayerModule, "setIntOwnerOnly", params, wallet, [owner]), TARGET_OF_DATA_NOT_WALLET_REVERT_MSG,
+      );
+    });
+
+    it("should fail to relay when _data is less than 36 bytes", async () => {
+      const params = []; // the first argument is not the wallet address, which should make the relaying rever
+      await assert.revertWith(
+        manager.relay(relayerModule, "clearInt", params, wallet, [owner]), INVALID_DATA_REVERT_MSG,
+      );
+    });
+
+    it("should fail to relay a duplicate transaction", async () => {
+      const params = [wallet.contractAddress, 2];
+      const nonce = await getNonceForRelay();
+      const relayParams = [relayerModule, "setIntOwnerOnly", params, wallet, [owner],
+        global.accounts[9].signer, false, 2000000, nonce];
+      await manager.relay(...relayParams);
+      await assert.revertWith(
+        manager.relay(...relayParams), DUPLICATE_REQUEST_REVERT_MSG,
+      );
+    });
+  });
+
+  describe("RelayerModuleV2", () => {
+    it("should fail to relay when target of _data != _wallet", async () => {
+      const params = [await getRandomAddress(), 4]; // the first argument is not the wallet address, which should make the relaying revert
+      await assert.revertWith(
+        manager.relay(relayerModuleV2, "setIntOwnerOnly", params, wallet, [owner]), TARGET_OF_DATA_NOT_WALLET_REVERT_MSG_V2,
+      );
+    });
+
+    it("should fail to relay when _data is less than 36 bytes", async () => {
+      const params = []; // the first argument is not the wallet address, which should make the relaying rever
+      await assert.revertWith(
+        manager.relay(relayerModuleV2, "clearInt", params, wallet, [owner]), INVALID_DATA_REVERT_MSG,
+      );
+    });
+
+    it("should fail to relay a duplicate transaction", async () => {
+      const params = [wallet.contractAddress, 2];
+      const nonce = await getNonceForRelay();
+      const relayParams = [relayerModule, "setIntOwnerOnly", params, wallet, [owner],
+        global.accounts[9].signer, false, 2000000, nonce];
+      await manager.relay(...relayParams);
+      await assert.revertWith(
+        manager.relay(...relayParams), DUPLICATE_REQUEST_REVERT_MSG,
+      );
+    });
+  });
+});
