1711fb4468e75fa6484c43a012988f806b1d0843
elenadimitrova <elena@arenabg.com>
2020-04-02 13:27:36 +0300

    Improve test coverage on the relayer

--- test/relayer.js
+++ test/relayer.js
@@ -1,8 +1,13 @@
+const ethers = require("ethers");
 const { AddressZero } = require("ethers").constants;
 
 const Wallet = require("../build/BaseWallet");
 const TestModule = require("../build/TestModule");
 const TestModuleRelayerV2 = require("../build/TestModuleRelayerV2");
+const Registry = require("../build/ModuleRegistry");
+const GuardianStorage = require("../build/GuardianStorage");
+const ApprovedTransfer = require("../build/ApprovedTransfer");
+const RecoveryManager = require("../build/RecoveryManager");
 
 const TestManager = require("../utils/test-manager");
 const { getRandomAddress } = require("../utils/utilities.js");
@@ -56,6 +61,51 @@ describe("RelayerModule", function () {
         manager.relay(...relayParams), DUPLICATE_REQUEST_REVERT_MSG,
       );
     });
+
+    it("should update the nonce after transaction", async () => {
+      const nonce = await getNonceForRelay();
+      await manager.relay(relayerModule, "setIntOwnerOnly", [wallet.contractAddress, 2], wallet, [owner],
+        global.accounts[9].signer, false, 2000000, nonce);
+
+      const updatedNonce = await relayerModule.getNonce(wallet.contractAddress);
+      const updatedNonceHex = await ethers.utils.hexZeroPad(updatedNonce.toHexString(), 32);
+      assert.equal(nonce, updatedNonceHex);
+    });
+
+    it("should not allow ApprovedTransfer and RecoveryManager module functions to be executed directly", async () => {
+      const registry = await deployer.deploy(Registry);
+      const guardianStorage = await deployer.deploy(GuardianStorage);
+      const approvedTransfer = await deployer.deploy(ApprovedTransfer, {}, registry.contractAddress, guardianStorage.contractAddress);
+      const recoveryManager = await deployer.deploy(RecoveryManager, {}, registry.contractAddress, guardianStorage.contractAddress, 36, 120, 24, 12);
+
+      const randomAddress = await getRandomAddress();
+
+      await assert.revertWith(
+        approvedTransfer.transferToken(wallet.contractAddress, randomAddress, randomAddress, 1, ethers.constants.HashZero),
+        "RM: must be called via execute()",
+      );
+
+      await assert.revertWith(
+        approvedTransfer.callContract(wallet.contractAddress, randomAddress, 1, ethers.constants.HashZero),
+        "RM: must be called via execute()",
+      );
+
+      await assert.revertWith(
+        approvedTransfer.approveTokenAndCallContract(
+          wallet.contractAddress,
+          randomAddress,
+          randomAddress,
+          1,
+          randomAddress,
+          ethers.constants.HashZero,
+        ),
+        "RM: must be called via execute()",
+      );
+
+      await assert.revertWith(recoveryManager.executeRecovery(wallet.contractAddress, randomAddress), "RM: must be called via execute()");
+      await assert.revertWith(recoveryManager.cancelRecovery(wallet.contractAddress), "RM: must be called via execute()");
+      await assert.revertWith(recoveryManager.transferOwnership(wallet.contractAddress, randomAddress), "RM: must be called via execute()");
+    });
   });
 
   describe("RelayerModuleV2", () => {
@@ -82,6 +132,16 @@ describe("RelayerModule", function () {
       await assert.revertWith(
         manager.relay(...relayParams), DUPLICATE_REQUEST_REVERT_MSG,
       );
+    });
+
+    it("should update the nonce after transaction", async () => {
+      const nonce = await getNonceForRelay();
+      await manager.relay(relayerModuleV2, "setIntOwnerOnly", [wallet.contractAddress, 2], wallet, [owner],
+        global.accounts[9].signer, false, 2000000, nonce);
+
+      const updatedNonce = await relayerModuleV2.getNonce(wallet.contractAddress);
+      const updatedNonceHex = await ethers.utils.hexZeroPad(updatedNonce.toHexString(), 32);
+      assert.equal(nonce, updatedNonceHex);
     });
   });
 });
