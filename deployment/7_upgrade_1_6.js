9f2d73575c5244a9cb1ad014262aa5d43b121860
elenadimitrova <elena@arenabg.com>
2020-04-08 13:58:34 +0300

    Remove config.modules.TokenTransfer and use current TransferManager address prior to upgrades, otherwise use 0x0

--- deployment/7_upgrade_1_6.js
+++ deployment/7_upgrade_1_6.js
@@ -112,7 +112,7 @@ const deploy = async (network) => {
     config.settings.securityPeriod || 0,
     config.settings.securityWindow || 0,
     config.settings.defaultLimit || "1000000000000000000",
-    ["test", "staging", "prod"].includes(network) ? config.modules.TokenTransfer : "0x0000000000000000000000000000000000000000",
+    ["test", "staging", "prod"].includes(network) ? config.modules.TransferManager : "0x0000000000000000000000000000000000000000",
   );
   newModuleWrappers.push(TransferManagerWrapper);
 
