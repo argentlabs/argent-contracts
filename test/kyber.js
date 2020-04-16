f622957ebbd2c5e3fedfb6d59703654076830276
Olivier van den Biggelaar <ovdbigge@gmail.com>
2020-04-16 17:27:40 +0100

    Importing ETH_TOKEN from utilities.js

--- test/kyber.js
+++ test/kyber.js
@@ -5,8 +5,8 @@ const KyberNetwork = require("../build/KyberNetworkTest");
 const ERC20 = require("../build/TestERC20");
 
 const TestManager = require("../utils/test-manager");
+const { ETH_TOKEN } = require("../utils/utilities.js");
 
-const ETH_TOKEN = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 const ERC20_SUPPLY = 10000000;
 const ERC20_DECIMALS = 18;
 const ERC20_RATE = 51 * 10 ** 13; // 1 ERC20 = 0.00051 ETH
