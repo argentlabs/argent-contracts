400c5fa1f9ea49e2a00390a9b57df63b086fc0e3
elenadimitrova <elena@arenabg.com>
2020-05-06 08:08:28 +0300

    Add a script for generating contracts api json

--- scripts/contracts_definition.js
+++ scripts/contracts_definition.js
@@ -1,3 +1,5 @@
+const ethers = require("ethers");
+const fs = require("fs");
 const TokenPriceProvider = require("../build/TokenPriceProvider");
 const WalletFactory = require("../build/WalletFactory");
 const ApprovedTransfer = require("../build/ApprovedTransfer");
@@ -9,29 +11,57 @@ const RecoveryManager = require("../build/RecoveryManager");
 const TokenExchanger = require("../build/TokenExchanger");
 const TransferManager = require("../build/TransferManager");
 
-const ethers = require("ethers");
-const fs = require("fs");
 
-[
+const contracts = [
   TokenPriceProvider,
-  // WalletFactory,
-  // ApprovedTransfer,
-  // CompoundManager,
-  // GuardianManager,
-  // LockManager,
-  // NftTransfer,
-  // RecoveryManager,
-  // TokenExchanger,
-  // TransferManager
-].forEach((contract) => {
-  // Use ethers.js Interface object :
-  // https://docs.ethers.io/ethers.js/html/api-advanced.html#interface
-  const contractInterface = new ethers.utils.Interface(contract.abi);
+  WalletFactory,
+  ApprovedTransfer,
+  CompoundManager,
+  GuardianManager,
+  LockManager,
+  NftTransfer,
+  RecoveryManager,
+  TokenExchanger,
+  TransferManager,
+];
 
-  contractInterface.interface.functions
-  .filter(functionDefinition => functionDefinition.type === "transaction")
-  .forEach(functionDefinition => {
-    console.log(contract.contractName);
-    console.log(functionDefinition);
+const jsonOutput = [];
+
+// Use ethers.js Interface object :
+// https://docs.ethers.io/ethers.js/html/api-advanced.html#interface
+contracts.forEach((contract) => {
+  // Contract interface object is richer in information hydrated by ethers, sample entry:
+  // _FunctionDescription {
+  //   inputs: [ { name: '_manager', type: 'address' } ],
+  //   outputs: [],
+  //   gas: undefined,
+  //   payable: false,
+  //   type: 'transaction',
+  //   name: 'addManager',
+  //   signature: 'addManager(address)',
+  //   sighash: '0x2d06177a' }
+  const contractInterface = new ethers.utils.Interface(contract.abi);
+  // Filters out from contract.abi functions, sample entry
+  // { constant: false,
+  //   inputs: [ { name: '_manager', type: 'address' } ],
+  //   name: 'addManager',
+  //   outputs: [],
+  //   payable: false,
+  //   stateMutability: 'nonpayable',
+  //   type: 'function' }
+  const functions = contract.abi.filter((functionDefinition) => functionDefinition.type === "function");
+  const nonStaticFunctions = functions.filter((functionDefinition) => functionDefinition.constant === false);
+
+  nonStaticFunctions.forEach((functionDefinition) => {
+    const entry = {
+      contract: contract.contractName,
+      name: functionDefinition.name,
+      signature: contractInterface.functions[functionDefinition.name].signature,
+      sighash: contractInterface.functions[functionDefinition.name].sighash,
+      gasLimit: contractInterface.functions[functionDefinition.name].gas,
+    };
+    jsonOutput.push(entry);
   });
 });
+
+fs.writeFileSync("eth_relayTransaction.json", JSON.stringify(jsonOutput, null, 2));
