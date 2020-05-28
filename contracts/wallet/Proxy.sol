173493ed0cd4604ec22463a974bf973e14471e0b
elenadimitrova <elena@arenabg.com>
2020-05-28 16:56:41 +0300

    Split the fallback function to receive

--- contracts/wallet/Proxy.sol
+++ contracts/wallet/Proxy.sol
@@ -33,20 +33,19 @@ contract Proxy {
     }
 
     fallback() external payable {
-
-        if (msg.data.length == 0 && msg.value > 0) {
-            emit Received(msg.value, msg.sender, msg.data);
-        } else {
-            // solium-disable-next-line security/no-inline-assembly
-            assembly {
-                let target := sload(0)
-                calldatacopy(0, 0, calldatasize())
-                let result := delegatecall(gas(), target, 0, calldatasize(), 0, 0)
-                returndatacopy(0, 0, returndatasize())
-                switch result
-                case 0 {revert(0, returndatasize())}
-                default {return (0, returndatasize())}
-            }
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            let target := sload(0)
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), target, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 {revert(0, returndatasize())}
+            default {return (0, returndatasize())}
         }
+    }
+
+    receive() external payable {
+        emit Received(msg.value, msg.sender, msg.data);
     }
 }
