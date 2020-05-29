722d2595ec11b65589fbe1bb58d6ead899db0d38
elenadimitrova <elena@arenabg.com>
2020-05-29 13:21:32 +0300

    Add missing mint function to ERC721 token test contract

--- contracts-test/TestERC721.sol
+++ contracts-test/TestERC721.sol
@@ -6,4 +6,8 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 contract TestERC721 is ERC721 {
     constructor() ERC721("Argent Kitties", "AGKT") public {
     }
+
+    function mint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
+    }
 }
