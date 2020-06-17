fb9be22d76a0c2eb4cd9254cfdc1836cd0bdbbd4
elenadimitrova <elena@arenabg.com>
2020-06-17 20:27:52 +0300

    Provide for tokens that don't implement decimals() property and also tokens that have decimals = 0

--- contracts/modules/common/TokenPriceProvider.sol
+++ contracts/modules/common/TokenPriceProvider.sol
@@ -24,15 +24,7 @@ contract TokenPriceProvider is Managed {
 
     mapping(address => uint256) public cachedPrices;
 
-    function setPrice(ERC20 _token, uint256 _price) public onlyManager {
-        cachedPrices[address(_token)] = _price;
-    }
-
-    function setPriceForTokenList(ERC20[] calldata _tokens, uint256[] calldata _prices) external onlyManager {
-        for (uint16 i = 0; i < _tokens.length; i++) {
-            setPrice(_tokens[i], _prices[i]);
-        }
-    }
+    event TokenDecimalsMissing(address indexed token);
 
     /**
      * @dev Converts the value of _amount tokens in ether.
@@ -41,8 +33,27 @@ contract TokenPriceProvider is Managed {
      * @return the ether value (in wei) of _amount tokens with contract _token
      */
     function getEtherValue(uint256 _amount, address _token) external view returns (uint256) {
-        uint256 decimals = ERC20(_token).decimals();
+        uint256 decimals;
+
+        try ERC20(_token).decimals() returns (uint _decimals) {
+            decimals = _decimals;
+        }
+        catch (bytes memory lowLevelData) {
+            emit TokenDecimalsMissing(_token);
+        }
+
         uint256 price = cachedPrices[_token];
-        return price.mul(_amount).div(10**decimals);
+        uint256 etherValue = decimals == 0 ? price.mul(_amount) : price.mul(_amount).div(10**decimals);
+        return etherValue;
+    }
+
+    function setPriceForTokenList(address[] calldata _tokens, uint256[] calldata _prices) external onlyManager {
+        for (uint16 i = 0; i < _tokens.length; i++) {
+            setPrice(_tokens[i], _prices[i]);
+        }
+    }
+
+    function setPrice(address _token, uint256 _price) public onlyManager {
+        cachedPrices[_token] = _price;
     }
 }
--- test/tokenPriceProvider.js
+++ test/tokenPriceProvider.js
@@ -1,11 +1,13 @@
 /* global accounts */
 
-const ERC20 = require("../build/TestERC20");
+const TestERC20 = require("../build/TestERC20");
+const DSTokenBase = require("../build/DSTokenBase");
 const TokenPriceProvider = require("../build/TokenPriceProvider");
 
 const TestManager = require("../utils/test-manager");
 
-describe("Token Price Provider", () => {
+describe("Token Price Provider", function () {
+  this.timeout(10000);
   const manager = new TestManager();
 
   const infrastructure = accounts[0].signer;
@@ -15,6 +17,8 @@ describe("Token Price Provider", () => {
   let priceProvider;
   let erc20First;
   let erc20Second;
+  let erc20ZeroDecimals;
+  let baseERC20;
 
   before(async () => {
     deployer = manager.newDeployer();
@@ -23,8 +27,11 @@ describe("Token Price Provider", () => {
   beforeEach(async () => {
     priceProvider = await deployer.deploy(TokenPriceProvider);
     await priceProvider.addManager(priceProviderManager.address);
-    erc20First = await deployer.deploy(ERC20, {}, [infrastructure.address], 10000000, 18);
-    erc20Second = await deployer.deploy(ERC20, {}, [infrastructure.address], 10000000, 18);
+    erc20First = await deployer.deploy(TestERC20, {}, [infrastructure.address], 10000000, 18);
+    erc20Second = await deployer.deploy(TestERC20, {}, [infrastructure.address], 10000000, 18);
+    erc20ZeroDecimals = await deployer.deploy(TestERC20, {}, [infrastructure.address], 10000000, 0);
+    await erc20ZeroDecimals._setup
+    baseERC20 = await deployer.deploy(DSTokenBase, {}, 10000000);
   });
 
   describe("Reading and writing token prices", () => {
@@ -46,6 +53,18 @@ describe("Token Price Provider", () => {
       await priceProvider.from(priceProviderManager).setPrice(erc20First.contractAddress, 1800);
       const etherValue = await priceProvider.getEtherValue("15000000000000000000", erc20First.contractAddress);
       assert.isTrue(etherValue.eq(1800 * 15));
+    });
+
+    it("should be able to get the ether value for a token with no decimals property", async () => {
+      await priceProvider.from(priceProviderManager).setPrice(baseERC20.contractAddress, "192297647000000000"); // Using a mock price for DGD token 
+      const etherValue = await priceProvider.getEtherValue(100, baseERC20.contractAddress);
+      assert.equal(etherValue.toString(), "19229764700000000000");
+    });
+
+    it("should be able to get the ether value for a token with 0 decimals", async () => {
+      await priceProvider.from(priceProviderManager).setPrice(erc20ZeroDecimals.contractAddress, 23000);
+      const etherValue = await priceProvider.getEtherValue(100, erc20ZeroDecimals.contractAddress);
+      assert.equal(etherValue.toString(), 2300000);
     });
   });
 });
