2a8308acb347043bb65b246b13d4da2dfd48bd05
elenadimitrova <elena@arenabg.com>
2020-06-25 17:07:13 +0300

    Add a test demonstrating issue with non compliant token recovery

--- test/baseModule.js
+++ test/baseModule.js
@@ -5,6 +5,7 @@ const Registry = require("../build/ModuleRegistry");
 const GuardianStorage = require("../build/GuardianStorage");
 const BaseModule = require("../build/BaseModule");
 const ERC20 = require("../build/TestERC20");
+const NonCompliantERC20 = require("../build/NonCompliantERC20");
 
 const TestManager = require("../utils/test-manager");
 
@@ -28,11 +29,12 @@ describe("BaseModule", function () {
     registry = await deployer.deploy(Registry);
     guardianStorage = await deployer.deploy(GuardianStorage);
     token = await deployer.deploy(ERC20, {}, [owner.address], 10, 18);
+
     const name = ethers.utils.formatBytes32String("BaseModule");
     baseModule = await deployer.deploy(BaseModule, {}, registry.contractAddress, guardianStorage.contractAddress, name);
   });
 
-  describe("Recover token", async () => {
+  describe("Recover tokens", async () => {
     it("should be able to recover ERC20 tokens sent to the module", async () => {
       let balance = await token.balanceOf(baseModule.contractAddress);
       assert.equal(balance, 0);
@@ -47,6 +49,26 @@ describe("BaseModule", function () {
       assert.equal(balance, 0);
 
       const moduleregistryBalance = await token.balanceOf(registry.contractAddress);
+      assert.equal(moduleregistryBalance, 10000000);
+    });
+
+    it.skip("should be able to recover non-ERC20 compliant tokens sent to the module", async () => {
+      const nonCompliantToken = await deployer.deploy(NonCompliantERC20, {});
+      await nonCompliantToken.mint(owner.address, 10000000);
+
+      let balance = await nonCompliantToken.balanceOf(baseModule.contractAddress);
+      assert.equal(balance, 0);
+
+      await nonCompliantToken.from(owner).transfer(baseModule.contractAddress, 10000000);
+      balance = await nonCompliantToken.balanceOf(baseModule.contractAddress);
+      assert.equal(balance, 10000000);
+
+      await baseModule.recoverToken(nonCompliantToken.contractAddress);
+
+      balance = await nonCompliantToken.balanceOf(baseModule.contractAddress);
+      assert.equal(balance, 0);
+
+      const moduleregistryBalance = await nonCompliantToken.balanceOf(registry.contractAddress);
       assert.equal(moduleregistryBalance, 10000000);
     });
   });
