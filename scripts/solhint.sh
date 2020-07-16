70a8f4515d58a6ecfeeb45141704245b230be642
elenadimitrova <elena@arenabg.com>
2020-07-16 15:02:51 +0300

    Replace ethlint with solhint config

--- scripts/solhint.sh
+++ scripts/solhint.sh
@@ -5,9 +5,9 @@ for file in $(git diff --cached --name-only | grep -E '^contracts/.*\.sol$')
 do
   if [ -f "$FILE" ]; then
     echo "Checking $file"
-    git show ":$file" | node_modules/.bin/solium --stdin "$file" # we only want to lint the staged changes, not any un-staged changes
+    git show ":$file" | node_modules/.bin/solhint --stdin "$file" # we only want to lint the staged changes, not any un-staged changes
     if [ $? -ne 0 ]; then
-      echo "ethlint failed on staged file '$file'."
+      echo "solhint failed on staged file '$file'."
       exit 1 # exit with failure status
     fi
   fi
