692c6e9af682391b22f9e1f0e71f523ec1a2e61c
elenadimitrova <elena@arenabg.com>
2020-07-16 13:02:29 +0300

    Refactoring coveredByDailyLimit checks in TransferManager for clarity

--- contracts/modules/TransferManager.sol
+++ contracts/modules/TransferManager.sol
@@ -248,7 +248,7 @@ contract TransferManager is OnlyOwnerModule, BaseTransfer {
     {
         if (!isWhitelisted(_wallet, _contract)) {
             // Make sure we don't call a supported ERC20 that's not whitelisted
-            notCoveredByDailyLimit(_wallet, _contract);
+            require(!coveredByDailyLimit(_wallet, _contract), "TM: Forbidden contract");
 
             require(LimitUtils.checkAndUpdateDailySpent(limitStorage, _wallet, _value), "TM: Call contract above daily limit");
         }
@@ -281,7 +281,7 @@ contract TransferManager is OnlyOwnerModule, BaseTransfer {
     {
         if (!isWhitelisted(_wallet, _spender)) {
             // Make sure we don't call a supported ERC20 that's not whitelisted
-            notCoveredByDailyLimit(_wallet, _contract);
+            require(!coveredByDailyLimit(_wallet, _contract), "TM: Forbidden contract");
             // check if the amount is under the daily limit
             // check the entire amount because the currently approved amount will be restored and should still count towards the daily limit
             uint256 valueInEth = LimitUtils.getEtherValue(tokenPriceStorage, _amount, _token);
@@ -530,10 +530,7 @@ contract TransferManager is OnlyOwnerModule, BaseTransfer {
     * @param _wallet The target wallet.
     * @param _contract The address of the contract.
      */
-    function notCoveredByDailyLimit(address _wallet, address _contract) internal view {
-        require(
-            tokenPriceStorage.getTokenPrice(_contract) == 0 ||
-            isLimitDisabled(_wallet),
-            "TM: Forbidden contract");
+    function coveredByDailyLimit(address _wallet, address _contract) internal view returns (bool) {
+        return (tokenPriceStorage.getTokenPrice(_contract) > 0 && !isLimitDisabled(_wallet));
     }
 }
