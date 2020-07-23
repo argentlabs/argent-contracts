95cb7478110b4e7525abcfca454ec134aa85b850
elenadimitrova <elena@arenabg.com>
2020-07-23 15:57:31 +0300

    Update ENSManager natspec in interface

--- contracts/infrastructure/ens/IENSManager.sol
+++ contracts/infrastructure/ens/IENSManager.sol
@@ -25,9 +25,37 @@ interface IENSManager {
     event Registered(address indexed _owner, string _ens);
     event Unregistered(string _ens);
 
+    /**
+     * @notice This function must be called when the ENS Manager contract is replaced
+     * and the address of the new Manager should be provided.
+     * @param _newOwner The address of the new ENS manager that will manage the root node.
+     */
     function changeRootnodeOwner(address _newOwner) external;
+
+    /**
+    * @notice Lets the manager assign an ENS subdomain of the root node to a target address.
+    * Registers both the forward and reverse ENS.
+    * @param _label The subdomain label.
+    * @param _owner The owner of the subdomain.
+    */
     function register(string calldata _label, address _owner) external;
+
+    /**
+     * @notice Returns true is a given subnode is available.
+     * @param _subnode The target subnode.
+     * @return true if the subnode is available.
+     */
     function isAvailable(bytes32 _subnode) external view returns(bool);
+
+    /**
+    * @notice Gets the official ENS reverse registrar.
+    * @return Address of the ENS reverse registrar.
+    */
     function getENSReverseRegistrar() external view returns (address);
+
+    /**
+    * @notice Gets the ENS Resolver.
+    * @return Address of the ENS resolver.
+    */
     function ensResolver() external view returns (address);
 }
