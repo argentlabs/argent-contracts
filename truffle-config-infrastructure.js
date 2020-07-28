61f0cf2350dbbab1a628960e389c4c973eaf0593
elenadimitrova <elena@arenabg.com>
2020-07-28 15:37:28 +0300

    Fix path in infrastructure contracts compilation

--- truffle-config-infrastructure.js
+++ truffle-config-infrastructure.js
@@ -34,7 +34,7 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-  contracts_directory: "contracts/modules",
+  contracts_directory: "contracts/infrastructure",
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
