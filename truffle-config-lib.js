b4fda7c6d919b89b970d5a3a64368e2765b621d4
elenadimitrova <elena@arenabg.com>
2020-07-28 15:05:59 +0300

    Switch contracts compile to use truffle

---
+++ truffle-config-lib.js
@@ -0,0 +1,27 @@
+module.exports = {
+  contracts_directory: "lib",
+  networks: {
+    development: {
+     host: "127.0.0.1",
+     port: 8545,
+     network_id: "*",
+    },
+  },
+
+  mocha: {
+    // timeout: 100000
+  },
+
+  compilers: {
+    solc: {
+      version: "0.5.4",    // Fetch exact version from solc-bin (default: truffle's version)
+      docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+      }
+    }
+  }
+}
