cfb31944aa5bb1da0cfdf2eb377296b1029306d8
elenadimitrova <elena@arenabg.com>
2020-08-18 11:57:10 +0300

    Set network id to 1597649375983 for development network

--- truffle-config-contracts-legacy-1.3.js
+++ truffle-config-contracts-legacy-1.3.js
@@ -46,7 +46,7 @@ module.exports = {
     development: {
       host: "127.0.0.1", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
-      network_id: "*", // Any network (default: none)
+      network_id: "1597649375983",
       gas: 20700000
     },
 
