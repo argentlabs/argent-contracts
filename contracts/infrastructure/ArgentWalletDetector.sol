73fd211160ca01cd22b1a22dc3a2a8fed209b27c
Julien Niset <julien.niset@gmail.com>
2020-10-16 12:36:15 +0200

    fixing indentation issues

--- contracts/infrastructure/ArgentWalletDetector.sol
+++ contracts/infrastructure/ArgentWalletDetector.sol
@@ -28,99 +28,99 @@ import "../wallet/IWallet.sol";
  */
 contract ArgentWalletDetector is Owned {
 	
-	// The accepted code hashes
-	bytes32[] private codes;
-	// The accepted implementations
-	address[] private implementations;
-	// mapping to efficiently check if a code is accepted
+    // The accepted code hashes
+    bytes32[] private codes;
+    // The accepted implementations
+    address[] private implementations;
+    // mapping to efficiently check if a code is accepted
     mapping (bytes32 => Info) public acceptedCodes;
-	// mapping to efficiently check is an implementation is accepted
-	mapping (address => Info) public acceptedImplementations;
+    // mapping to efficiently check is an implementation is accepted
+    mapping (address => Info) public acceptedImplementations;
 
-	struct Info {
+    struct Info {
         bool exists;
         uint128 index;
     }
 
-	// emits when a new accepted code is added
-	event CodeAdded(bytes32 indexed code);
-	// emits when a new accepted implementation is added 
-	event ImplementationAdded(address indexed implementation);
+    // emits when a new accepted code is added
+    event CodeAdded(bytes32 indexed code);
+    // emits when a new accepted implementation is added 
+    event ImplementationAdded(address indexed implementation);
 
-	constructor(bytes32[] memory _codes, address[] memory _implementations) public {
-		for(uint i = 0; i < _codes.length; i++) {
-			addCode(_codes[i]);
-		}
-		for(uint j = 0; j < _implementations.length; j++) {
-			addImplementation(_implementations[j]);
-		}
-	}
+    constructor(bytes32[] memory _codes, address[] memory _implementations) public {
+        for(uint i = 0; i < _codes.length; i++) {
+            addCode(_codes[i]);
+        }
+        for(uint j = 0; j < _implementations.length; j++) {
+            addImplementation(_implementations[j]);
+        }
+    }
 
-	/**
+    /**
     * @notice Adds a new acceted code hash.
     * @param _code The new code hash.
     */
-	function addCode(bytes32 _code) public onlyOwner {
+    function addCode(bytes32 _code) public onlyOwner {
         require(_code != bytes32(0), "AWR: empty _code");
         Info storage code = acceptedCodes[_code];
-		if(!code.exists) {
-			codes.push(_code);
-			code.exists = true;
-        	code.index = uint128(codes.length - 1);
-			emit CodeAdded(_code);
-		}
+        if(!code.exists) {
+            codes.push(_code);
+            code.exists = true;
+            code.index = uint128(codes.length - 1);
+            emit CodeAdded(_code);
+        }
     }
 	
-	/**
+    /**
     * @notice Adds a new acceted implementation.
     * @param _impl The new implementation.
     */
-	function addImplementation(address _impl) public onlyOwner {
+    function addImplementation(address _impl) public onlyOwner {
         require(_impl != address(0), "AWR: empty _impl");
         Info storage impl = acceptedImplementations[_impl];
-		if(!impl.exists) {
-			implementations.push(_impl);
-			impl.exists = true;
-        	impl.index = uint128(implementations.length - 1);
-			emit ImplementationAdded(_impl);
-		}
+        if(!impl.exists) {
+            implementations.push(_impl);
+            impl.exists = true;
+            impl.index = uint128(implementations.length - 1);
+            emit ImplementationAdded(_impl);
+        }
     }
 
-	/**
+    /**
     * @notice Adds a new acceted code hash and implementation from a deployed Argent wallet.
     * @param _argentWallet The deployed Argent wallet.
     */
     function addCodeAndImplementationFromWallet(address _argentWallet) external onlyOwner {
         bytes32 codeHash;   
-		// solhint-disable-next-line no-inline-assembly
-    	assembly { codeHash := extcodehash(_argentWallet) }
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codeHash := extcodehash(_argentWallet) }
         addCode(codeHash);
         address implementation = IWallet(_argentWallet).implementation(); 
         addImplementation(implementation);
     }
 
-	/**
+    /**
     * @notice Gets the list of accepted implementations.
     */
-	function getImplementations() public view returns (address[] memory) {
-		return implementations;
-	}
+    function getImplementations() public view returns (address[] memory) {
+        return implementations;
+    }
 
-	/**
+    /**
     * @notice Gets the list of accepted code hash.
     */
-	function getCodes() public view returns (bytes32[] memory) {
-		return codes;
-	}
+    function getCodes() public view returns (bytes32[] memory) {
+        return codes;
+    }
 
-	/**
+    /**
     * @notice Checks if an address is an Argent wallet
-	* @param _wallet The target wallet
+    * @param _wallet The target wallet
     */
-	function isArgentWallet(address _wallet) external view returns (bool) {
-		bytes32 codeHash;    
-		// solhint-disable-next-line no-inline-assembly
-    	assembly { codeHash := extcodehash(_wallet) }
-		return acceptedCodes[codeHash].exists && acceptedImplementations[IWallet(_wallet).implementation()].exists;
-	}
+    function isArgentWallet(address _wallet) external view returns (bool) {
+        bytes32 codeHash;    
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codeHash := extcodehash(_wallet) }
+        return acceptedCodes[codeHash].exists && acceptedImplementations[IWallet(_wallet).implementation()].exists;
+    }
 }
