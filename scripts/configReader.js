b5f3db5c756a0f8df02523e5e022981e122ba531
elenadimitrova <elena@arenabg.com>
2020-10-19 10:48:22 +0300

    Update script format to match the changes for execute_script from PR #161

--- scripts/configReader.js
+++ scripts/configReader.js
@@ -1,6 +1,13 @@
-// Example Usage:
-// from the project root (as we're loading .env file from root via `dotenv`)
-// bash ./scripts/execute.sh scripts/configReader.js staging
+// ///////////////////////////////////////////////////////////////////
+// Script to print environment configuration from AWS.
+//
+// Can be executed (from the project root as we're loading .env file from root via `dotenv`) as:
+// bash ./scripts/execute_script.sh --no-compile scripts/configReader.js <network>
+//
+// where:
+//     - network = [test, staging, prod]
+// note: ganache configuration in solution under ./utils/config/ganache.json
+// ////////////////////////////////////////////////////////////////////
 
 require("dotenv").config();
 
@@ -15,16 +22,16 @@ async function main() {
   const env = process.env.CONFIG_ENVIRONMENT;
   const remotelyManagedNetworks = (process.env.S3_BUCKET_SUFFIXES || "").split(":");
 
-  let configLoader;
-  if (remotelyManagedNetworks.includes(network)) {
-    const bucket = `${process.env.S3_BUCKET_PREFIX}-${network}`;
-    const key = process.env.S3_CONFIG_KEY;
-    configLoader = new ConfiguratorLoader.S3(bucket, key);
-  } else {
-    const fileName = env ? `${network}.${env}.json` : `${network}.json`;
-    const filePath = path.join(__dirname, "./config", fileName);
-    configLoader = new ConfiguratorLoader.Local(filePath);
+  // Ensure a supported network is requested
+  if (!remotelyManagedNetworks.includes(network)) {
+    console.error("Error: Invalid network selected");
+    return;
   }
+
+  const bucket = `${process.env.S3_BUCKET_PREFIX}-${network}`;
+  const key = process.env.S3_CONFIG_KEY;
+  const configLoader = new ConfiguratorLoader.S3(bucket, key);
+
   const configurator = new Configurator(configLoader);
   await configurator.load();
   const configuration = configurator.copyConfig();
