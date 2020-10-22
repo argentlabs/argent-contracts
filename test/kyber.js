985ecfbf6b6fd138f38354b41d176103bb91b718
elenadimitrova <elena@arenabg.com>
2020-10-22 15:20:08 +0300

    Fix kyber and lockManager tests

--- test/kyber.js
+++ test/kyber.js
@@ -4,6 +4,13 @@ const ERC20 = artifacts.require("TestERC20");
 
 const { ETH_TOKEN, getBalance } = require("../utils/utilities.js");
 
+const chai = require("chai");
+const BN = require("bn.js");
+const bnChai = require("bn-chai");
+
+const { expect } = chai;
+chai.use(bnChai(BN));
+
 const ERC20_SUPPLY = 10000000;
 const ERC20_DECIMALS = 18;
 const ERC20_RATE = 51 * 10 ** 13; // 1 ERC20 = 0.00051 ETH
@@ -28,38 +35,44 @@ contract("KyberNetwork", (accounts) => {
   it("should exchange ETH for ERC20", async () => {
     const beforeERC20 = await erc20.balanceOf(trader);
     const beforeETH = await getBalance(trader);
-    assert.equal(beforeERC20.toNumber(), 0, "trader should have no ERC20");
+    // trader should have no ERC20
+    expect(beforeERC20).to.be.zero;
     await kyber.trade(ETH_TOKEN, 10000, erc20.address, trader,
-      new BigNumber("10000000000000000000000"), 1, "0x0000000000000000000000000000000000000000", { value: 10000, from: trader });
+      new BN("10000000000000000000000"), 1, "0x0000000000000000000000000000000000000000", { value: 10000, from: trader });
     const afterERC20 = await erc20.balanceOf(trader);
     const afterETH = await getBalance(trader);
-    assert.equal(beforeETH.sub(afterETH).gt(10000), true, "trader should have exchanged 10000 wei");
-    assert.equal(afterERC20.gt(0), true, "trader should have received ERC20");
+    // trader should have exchanged 10000 wei
+    expect(beforeETH.sub(afterETH)).to.be.gt.BN(10000);
+    // trader should have received ERC20
+    expect(afterERC20).to.be.gt.BN(0);
   });
 
   it("should exchange ERC20 for ETH", async () => {
     // provision ERC20 to trader
     await kyber.trade(
       ETH_TOKEN,
-      new BigNumber("1000000000000000000"),
+      new BN("1000000000000000000"),
       erc20.address,
       trader,
-      new BigNumber("10000000000000000000000"),
+      new BN("10000000000000000000000"),
       1,
       "0x0000000000000000000000000000000000000000",
-      { value: new BigNumber("1000000000000000000") },
+      { value: new BN("1000000000000000000") },
     );
     const beforeERC20 = await erc20.balanceOf(trader);
     const beforeETH = await getBalance(trader);
-    assert.equal(beforeERC20 > 0, true, "trader should have some ERC20");
+    // trader should have some ERC20
+    expect(beforeERC20).to.be.gt.BN(0);
     // exchange ERC20
-    const srcAmount = beforeERC20.div(new BigNumber(2));
+    const srcAmount = beforeERC20.div(new BN(2));
     await erc20.approve(kyber.address, srcAmount, { from: trader });
     await kyber.trade(erc20.address, srcAmount, ETH_TOKEN, trader,
-      new BigNumber("10000000000000000000000"), 1, "0x0000000000000000000000000000000000000000", { from: trader });
+      new BN("10000000000000000000000"), 1, "0x0000000000000000000000000000000000000000", { from: trader });
     const afterERC20 = await erc20.balanceOf(trader);
     const afterETH = await getBalance(trader);
-    assert.equal(beforeERC20.sub(afterERC20).eq(srcAmount), true, "trader should have exchanged ERC20");
-    assert.equal(afterETH.sub(beforeETH).gt(0), true, "trader should have received wei");
+    // trader should have exchanged ERC20
+    expect(beforeERC20.sub(afterERC20)).to.eq.BN(srcAmount);
+    // trader should have received wei
+    expect(afterETH.sub(beforeETH)).to.be.gt.BN(0);
   });
 });
