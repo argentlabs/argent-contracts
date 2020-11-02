2195dd2fd841ff64e804bdae686b902328d68b0f
elenadimitrova <elena@arenabg.com>
2020-11-02 09:39:48 +0200

    Consolidate truffle configuration in config base file

--- truffle-config-lib.js
+++ truffle-config-lib.js
@@ -1,23 +1,14 @@
-module.exports = {
-  contracts_directory: "lib",
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "1597649375983",
-      gas: 20700000
-    },
-  },
+const baseConfig = require("./truffle-config.base.js");
 
-  mocha: {
-    timeout: 100000,
-  },
+module.exports = {
+  ...baseConfig,
+  contracts_directory: "lib",
 
   compilers: {
     solc: {
-      version: "0.5.4", // Fetch exact version from solc-bin (default: truffle's version)
-      docker: true, // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: { // See the solidity docs for advice about optimization and evmVersion
+      version: "0.5.4",
+      docker: true,
+      settings: {
         optimizer: {
           enabled: true,
           runs: 200,
