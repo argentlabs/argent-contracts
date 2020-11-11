elenadimitrova/argent-contracts-1

Refactor .from syntax to truffle variant {from:}

test/recoveryManager.js
@@ -84,7 +84,7 @@ contract("RecoveryManager", (accounts) => {
     });
 
     for (const address of guardianAddresses) {
-      await guardianManager.from(owner).addGuardian(wallet.address, address);
+      await guardianManager.addGuardian(wallet.address, address, { from: owner });
     }
 
     await increaseTime(30);
@@ -446,7 +446,7 @@ contract("RecoveryManager", (accounts) => {
           ethers.constants.AddressZero,
         );
         await assert.revertWith(
-          relayerModule.from(nonowner2).execute(
+          relayerModule.execute(
             wallet.address,
             recoveryManager.address,
             methodData,
@@ -456,7 +456,7 @@ contract("RecoveryManager", (accounts) => {
             700000,
             ETH_TOKEN,
             ethers.constants.AddressZero,
-            { gasLimit: 800000 },
+            { gasLimit: 800000, from: nonowner2 },
           ),
           "RM: unknown method",
         );

test/relayer.js
@@ -298,7 +298,7 @@ contract("RelayerModule", (accounts) => {
       assert.isTrue(dailySpent.toNumber() === 10, "initial daily spent should be 10");
       const rBalanceStart = await getBalance(recipient);
       // add a guardian
-      await guardianManager.from(owner).addGuardian(wallet.address, guardian);
+      await guardianManager.addGuardian(wallet.address, guardian, { from: owner });
       // call approvedTransfer
       const params = [wallet.address, ETH_TOKEN, recipient, 1000, ethers.constants.HashZero];
       const nonce = await getNonceForRelay();
@@ -351,7 +351,7 @@ contract("RelayerModule", (accounts) => {
 
     it("should succeed when called directly on OnlyOwnerModule modules", async () => {
       await registry.registerModule(testModuleNew.address, formatBytes32String("testModuleNew"));
-      await testOnlyOwnerModule.from(owner).addModule(wallet.address, testModuleNew.address);
+      await testOnlyOwnerModule.addModule(wallet.address, testModuleNew.address, { from: owner });
 
       const isModuleAuthorised = await wallet.authorised(testModuleNew.address);
       assert.isTrue(isModuleAuthorised);
@@ -371,13 +371,13 @@ contract("RelayerModule", (accounts) => {
 
     it("should succeed when called directly on non-OnlyOwnerModule modules", async () => {
       await registry.registerModule(testModuleNew.address, formatBytes32String("testModuleNew"));
-      await approvedTransfer.from(owner).addModule(wallet.address, testModuleNew.address);
+      await approvedTransfer.addModule(wallet.address, testModuleNew.address, { from: owner });
       const isModuleAuthorised = await wallet.authorised(testModuleNew.address);
       assert.isTrue(isModuleAuthorised);
     });
 
     it("should fail to add module which is not registered", async () => {
-      await assert.revertWith(approvedTransfer.from(owner).addModule(wallet.address, testModuleNew.address),
+      await assert.revertWith(approvedTransfer.addModule(wallet.address, testModuleNew.address, { from: owner }),
         "BM: module is not registered");
     });
   });

test/tokenExchanger.js
@@ -274,7 +274,7 @@ contract("TokenExchanger", (accounts) => {
       const { success } = (await parseLogs(txR, relayerModule, "TransactionExecuted"))[0];
       assert.isTrue(success, "Relayed tx should succeed");
     } else {
-      txR = await (await exchanger.from(owner)[method](...params, { gasLimit: 2000000 })).wait();
+      txR = await (await exchanger[method](...params, { gasLimit: 2000000, from: owner })).wait();
     }
     const { destAmount } = (await parseLogs(txR, exchanger, "TokenExchanged"))[0];
 
@@ -344,7 +344,7 @@ contract("TokenExchanger", (accounts) => {
         fixedAmount,
         variableAmount,
       });
-      await assert.revertWith(exchangerExcludingAllExchanges.from(owner)[method](...params, { gasLimit: 2000000 }), "TE: Unauthorised Exchange");
+      await assert.revertWith(exchangerExcludingAllExchanges[method](...params, { gasLimit: 2000000, from: owner }), "TE: Unauthorised Exchange");
     });
 
     it(`lets old wallets call ${method} successfully`, async () => {
@@ -367,7 +367,7 @@ contract("TokenExchanger", (accounts) => {
 
     const testTradeWithPreExistingAllowance = async (allowance) => {
       const spender = await paraswap.getTokenTransferProxy();
-      await transferManager.from(owner).approveToken(wallet.address, tokenA.address, spender, allowance);
+      await transferManager.approveToken(wallet.address, tokenA.address, spender, allowance, { from: owner });
       // call sell
       await testTrade({
         method, fromToken: tokenA.address, toToken: ETH_TOKEN, relayed: false,

utils/relay-manager.js
@@ -44,7 +44,7 @@ class RelayManager {
       );
       return gasUsed;
     }
-    const tx = await this.relayerModule.from(_relayer).execute(
+    const tx = await this.relayerModule.execute(
       _wallet.address,
       _module.address,
       methodData,
@@ -54,7 +54,7 @@ class RelayManager {
       _gasLimit,
       _refundToken,
       _refundAddress,
-      { gasLimit: _gasLimitRelay, gasPrice: _gasPrice },
+      { gasLimit: _gasLimitRelay, gasPrice: _gasPrice, from: _relayer },
     );
     const txReceipt = await _module.verboseWaitForTransaction(tx);
     return txReceipt;

argentlabs/argent-contracts

test/recoveryManager.js
@@ -110,7 +110,7 @@ contract("RecoveryManager", (accounts) => {
     });
 
     for (const address of guardianAddresses) {
-      await guardianManager.from(owner).addGuardian(wallet.address, address);
+      await guardianManager.addGuardian(wallet.address, address, { from: owner });
     }
 
     await increaseTime(30);
@@ -487,7 +487,7 @@ contract("RecoveryManager", (accounts) => {
           ethers.constants.AddressZero,
         );
         await assert.revertWith(
-          relayerManager.from(nonowner2).execute(
+          relayerManager.execute(
             wallet.address,
             recoveryManager.address,
             methodData,
@@ -497,7 +497,7 @@ contract("RecoveryManager", (accounts) => {
             700000,
             ETH_TOKEN,
             ethers.constants.AddressZero,
-            { gasLimit: 800000 },
+            { gasLimit: 800000, from: nonowner2 },
           ),
           "RM: unknown method",
         );

test/relayer.js
@@ -345,7 +345,7 @@ contract("RelayerManager", (accounts) => {
       assert.isTrue(dailySpent.toNumber() === 10, "initial daily spent should be 10");
       const rBalanceStart = await getBalance(recipient);
       // add a guardian
-      await guardianManager.from(owner).addGuardian(wallet.address, guardian);
+      await guardianManager.addGuardian(wallet.address, guardian, { from: owner });
       // call approvedTransfer
       const params = [wallet.address, ETH_TOKEN, recipient, 1000, ethers.constants.HashZero];
       const nonce = await getNonceForRelay();
@@ -399,7 +399,7 @@ contract("RelayerManager", (accounts) => {
 
     it("should succeed when called directly on VersionManager", async () => {
       await registry.registerModule(versionManagerV2.address, formatBytes32String("versionManagerV2"));
-      await versionManager.from(owner).addModule(wallet.address, versionManagerV2.address);
+      await versionManager.addModule(wallet.address, versionManagerV2.address, { from: owner });
 
       const isModuleAuthorised = await wallet.authorised(versionManagerV2.address);
       assert.isTrue(isModuleAuthorised);
@@ -407,7 +407,7 @@ contract("RelayerManager", (accounts) => {
     });
 
     it("should fail to add module which is not registered", async () => {
-      await assert.revertWith(versionManager.from(owner).addModule(wallet.address, versionManagerV2.address),
+      await assert.revertWith(versionManager.addModule(wallet.address, versionManagerV2.address, { from: owner }),
         "VM: module is not registered");
     });
   });

test/tokenExchanger.js
@@ -300,7 +300,7 @@ contract("TokenExchanger", (accounts) => {
       const { success } = (await parseLogs(txR, relayerManager, "TransactionExecuted"))[0];
       assert.isTrue(success, "Relayed tx should succeed");
     } else {
-      txR = await (await exchanger.from(owner)[method](...params, { gasLimit: 2000000 })).wait();
+      txR = await (await exchanger[method](...params, { gasLimit: 2000000, from: owner })).wait();
     }
     const { destAmount } = (await parseLogs(txR, exchanger, "TokenExchanged"))[0];
 
@@ -382,7 +382,7 @@ contract("TokenExchanger", (accounts) => {
         fixedAmount,
         variableAmount,
       });
-      await assert.revertWith(exchanger.from(owner)[method](...params, { gasLimit: 2000000 }), "DR: Unauthorised DEX");
+      await assert.revertWith(exchanger[method](...params, { gasLimit: 2000000, from: owner }), "DR: Unauthorised DEX");
       // reset whitelist
       await dexRegistry.setAuthorised([kyberAdapter.address, uniswapV2Adapter.address], [true, true]);
     });
@@ -408,7 +408,7 @@ contract("TokenExchanger", (accounts) => {
 
     const testTradeWithPreExistingAllowance = async (allowance) => {
       const spender = await paraswap.getTokenTransferProxy();
-      await transferManager.from(owner).approveToken(wallet.address, tokenA.address, spender, allowance);
+      await transferManager.approveToken(wallet.address, tokenA.address, spender, allowance, { from: owner });
       // call sell
       await testTrade({
         method, fromToken: tokenA.address, toToken: ETH_TOKEN, relayed: false,

utils/relay-manager.js
@@ -46,7 +46,7 @@ class RelayManager {
       );
       return gasUsed;
     }
-    const tx = await this.relayerManager.from(_relayer).execute(
+    const tx = await this.relayerManager.execute(
       _wallet.address,
       _module.address,
       methodData,
@@ -56,7 +56,7 @@ class RelayManager {
       _gasLimit,
       _refundToken,
       _refundAddress,
-      { gasLimit: _gasLimitRelay, gasPrice: _gasPrice },
+      { gasLimit: _gasLimitRelay, gasPrice: _gasPrice, from: _relayer },
     );
     const txReceipt = await _module.verboseWaitForTransaction(tx);
     return txReceipt;