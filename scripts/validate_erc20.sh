e9d2caeb940ed7ece3b0393df1cfab2d9a9c1342
elenadimitrova <elena@arenabg.com>
2020-11-12 17:36:12 +0200

    Update script to a working version to use solc-select

--- scripts/validate_erc20.sh
+++ scripts/validate_erc20.sh
@@ -5,15 +5,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 mkdir "temp"
 
-priceProviderTokens=$(curl 'https://cloud.argent-api.com/v1/tokens/dailyLimit' | jq -r '.tokens[0] .address')
-echo $priceProviderTokens
+for tokenAddress in $(curl -s 'https://cloud.argent-api.com/v1/tokens/dailyLimit' | jq -r '.tokens[] | select(.address != "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") | .address')
+do
+    echo "${tokenAddress}"
 
-url="https://api.etherscan.io/api?module=contract&action=getsourcecode&address=$priceProviderTokens&apikey=$ETHERSCAN_API_KEY"
-resultEtherScan=$(curl $url | jq '.result[0]')
+    url="https://api.etherscan.io/api?module=contract&action=getsourcecode&address=$tokenAddress&apikey=$ETHERSCAN_API_KEY"
+    resultEtherScan=$(curl -s $url | jq '.result[0]')
 
-contractName=$(jq '.ContractName'<<<"$resultEtherScan")
-compilerVersion=$(jq '.CompilerVersion'<<<"$resultEtherScan")
+    contractName=$(jq -r '.ContractName'<<<"$resultEtherScan")
+    compilerVersionString=$(jq -r '.CompilerVersion | .[1:]'<<<"$resultEtherScan")
+    compilerVersion=$(cut -d"+" -f1<<<"$compilerVersionString")
+    echo $compilerVersion
 
-(jq -r '.SourceCode'<<<"$resultEtherScan") > "./temp/$contractName.sol"
+    (jq -r '.SourceCode'<<<"$resultEtherScan") > "./temp/$contractName.sol"
 
-slither-check-erc "./temp/$contractName.sol" "$contractName" --erc 20
+    SOLC_VERSION=$compilerVersion slither-check-erc "./temp/$contractName.sol" "$contractName" --erc ERC20
+
+done
+rm -r "temp"
