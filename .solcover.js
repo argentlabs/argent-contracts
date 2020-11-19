7c6c883ffffcb5ed01fb8974ed30d95395264a6b
elenadimitrova <elena@arenabg.com>
2020-11-19 16:48:20 +0200

    Update .solcover options for ganache

--- .solcover.js
+++ .solcover.js
@@ -1,27 +1,10 @@
-// ganache-cli \
-//  --chainId 1895 \ (core-option: _chainId)
-//  --networkId 1597649375983 \ (core-option: network_id)
-//  --gasLimit=20700000 \ (solidity-coverage sets this)
-//  -e 10000 \ (default_balance_ether)
-//  --acctKeys=\"./ganache-accounts.json\" (core-option: accounts)
-//  --deterministic", (maybe hd_path?)
-
-const ganacheAccounts = require('./ganache-accounts.json');
-let accounts = [];
-
-for (address of Object.keys(ganacheAccounts.private_keys)){
-  accounts.push({
-    privateKey: ganacheAccounts.private_keys[address],
-    balance: ganacheAccounts.addresses[address].account.balance
-  })
-}
-
 module.exports = {
   client: require('ganache-cli'),
-  skipFiles: ['Migrations.sol'],
+  skipFiles: [],
   providerOptions: {
-    accounts: accounts,
     _chainId: 1895,
-    network_id: 1597649375983
+    network_id: 1597649375983,
+    account_keys_path: "./ganache-accounts.json",
+    default_balance_ether: 10000
   }
 };
