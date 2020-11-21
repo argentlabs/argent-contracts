elenadimitrova/argent-contracts-1

Fix linting issues

test/baseWallet.js
@@ -36,7 +36,7 @@ contract("BaseWallet", (accounts) => {
 
   beforeEach(async () => {
     const proxy = await Proxy.new(walletImplementation.address);
-    wallet = BaseWallet.at(proxy.address);
+    wallet = await BaseWallet.at(proxy.address);
   });
 
   describe("Registering modules", () => {
@@ -101,15 +101,15 @@ contract("BaseWallet", (accounts) => {
 
       it("should not reinitialize a wallet", async () => {
         await wallet.init(owner, [module1.address]);
-        await utils.assertRevert(wallet.init(owner, [module1.address]), "BW: wallet already initialised");
+        await assertRevert(wallet.init(owner, [module1.address]), "BW: wallet already initialised");
       });
 
       it("should not initialize a wallet with no module", async () => {
-        await utils.assertRevert(wallet.init(owner, []), "BW: construction requires at least 1 module");
+        await assertRevert(wallet.init(owner, []), "BW: construction requires at least 1 module");
       });
 
       it("should not initialize a wallet with duplicate modules", async () => {
-        await utils.assertRevert(wallet.init(owner, [module1.address, module1.address]), "BW: module is already added");
+        await assertRevert(wallet.init(owner, [module1.address, module1.address]), "BW: module is already added");
       });
     });
 
@@ -132,12 +132,12 @@ contract("BaseWallet", (accounts) => {
     describe("Authorisations", () => {
       it("should not let a non-module deauthorise a module", async () => {
         await wallet.init(owner, [module1.address]);
-        await utils.assertRevert(wallet.authoriseModule(module1.address, false), "BW: msg.sender not an authorized module");
+        await assertRevert(wallet.authoriseModule(module1.address, false), "BW: msg.sender not an authorized module");
       });
 
       it("should not let a module set the owner to address(0)", async () => {
         await wallet.init(owner, [module1.address]);
-        await utils.assertRevert(module1.invalidOwnerChange(wallet.address), "BW: address cannot be null");
+        await assertRevert(module1.invalidOwnerChange(wallet.address), "BW: address cannot be null");
       });
     });
 
@@ -161,7 +161,7 @@ contract("BaseWallet", (accounts) => {
         assert.equal(module1IsAuthorised, true, "module1 should be authorised");
         const module2IsAuthorised = await wallet.authorised(module2.address);
         assert.equal(module2IsAuthorised, false, "module2 should not be authorised");
-        await utils.assertRevert(module1.enableStaticCalls(wallet.address, module2.address),
+        await assertRevert(module1.enableStaticCalls(wallet.address, module2.address),
           "BW: must be an authorised module for static call");
       });
 
@@ -179,7 +179,7 @@ contract("BaseWallet", (accounts) => {
 
         // trying to execute static call delegated to module1 (it should fail)
         const walletAsModule = await TestModule.at(wallet.address);
-        await utils.assertRevert(walletAsModule.contract.getBoolean(), "BW: must be an authorised module for static call");
+        await assertRevert(walletAsModule.contract.getBoolean(), "BW: must be an authorised module for static call");
       });
     });
   });

argentlabs/argent-contracts

test/baseWallet.js
@@ -56,7 +56,7 @@ contract("BaseWallet", (accounts) => {
 
   beforeEach(async () => {
     const proxy = await Proxy.new(walletImplementation.address);
-    wallet = BaseWallet.at(proxy.address);
+    wallet = await BaseWallet.at(proxy.address);
   });
 
   describe("Registering modules", () => {
@@ -125,16 +125,15 @@ contract("BaseWallet", (accounts) => {
       it("should not reinitialize a wallet", async () => {
         await wallet.init(owner, [module1.address]);
         await module1.upgradeWallet(wallet.address, await module1.lastVersion(), { from: owner });
-        
-        await utils.assertRevert(wallet.init(owner, [module1.address]), "BW: wallet already initialised");
+        await assertRevert(wallet.init(owner, [module1.address]), "BW: wallet already initialised");
       });
 
       it("should not initialize a wallet with no module", async () => {
-        await utils.assertRevert(wallet.init(owner, []), "BW: construction requires at least 1 module");
+        await assertRevert(wallet.init(owner, []), "BW: construction requires at least 1 module");
       });
 
       it("should not initialize a wallet with duplicate modules", async () => {
-        await utils.assertRevert(wallet.init(owner, [module1.address, module1.address]), "BW: module is already added");
+        await assertRevert(wallet.init(owner, [module1.address, module1.address]), "BW: module is already added");
       });
     });
 
@@ -157,14 +156,14 @@ contract("BaseWallet", (accounts) => {
     describe("Authorisations", () => {
       it("should not let a non-module deauthorise a module", async () => {
         await wallet.init(owner, [module1.address]);
-        await utils.assertRevert(wallet.authoriseModule(module1.address, false), "BW: msg.sender not an authorized module");
+        await assertRevert(wallet.authoriseModule(module1.address, false), "BW: msg.sender not an authorized module");
       });
 
       it("should not let a feature set the owner to address(0)", async () => {
         await wallet.init(owner, [module1.address]);
         await module1.upgradeWallet(wallet.address, await module1.lastVersion(), { from: owner });
 
-        await assert.revertWith(feature1.invalidOwnerChange(wallet.address), "BW: address cannot be null");
+        await assertRevert(feature1.invalidOwnerChange(wallet.address), "BW: address cannot be null");
       });
     });
 
@@ -199,7 +198,7 @@ contract("BaseWallet", (accounts) => {
 
         // trying to execute static call delegated to module1 (it should fail)
         const walletAsModule = await TestFeature.at(wallet.address);
-        await utils.assertRevert(walletAsModule.contract.getBoolean(), "BW: must be an authorised module for static call");
+        await assertRevert(walletAsModule.contract.getBoolean(), "BW: must be an authorised module for static call");
       });
     });
   });