4a5eafa2e4cc6312eccf5a6467a0335f50a8277c
Olivier van den Biggelaar <ovdbigge@gmail.com>
2021-03-02 09:58:10 +0000

    Fixing TestFilter

--- contracts-test/TestFilter.sol
+++ contracts-test/TestFilter.sol
@@ -20,7 +20,7 @@ import "../contracts/infrastructure/dapp/IFilter.sol";
 
 contract TestFilter is IFilter {
     function isValid(address /*_wallet*/, address /*_spender*/, address /*_to*/, bytes calldata _data) external override view returns (bool) {
-        (bytes32, uint256 state) = abi.decode(abi.encodePacked(bytes28(0),_data), (bytes32, uint256));
+        uint256 state = abi.decode(_data[4:], (uint256));
         return state != 5;
     }
 }
