8b25e40e036a0e73ee3707b2890cbeef87fc4372
Olivier van den Biggelaar <ovdbigge@gmail.com>
2021-03-02 09:49:11 +0000

    Resolving warnings

--- contracts/infrastructure/dapp/ParaswapFilter.sol
+++ contracts/infrastructure/dapp/ParaswapFilter.sol
@@ -1,3 +1,19 @@
+// Copyright (C) 2021  Argent Labs Ltd. <https://argent.xyz>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.s
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
@@ -52,7 +68,7 @@ contract ParaswapFilter is IFilter {
         tokenPriceRegistry = _tokenPriceRegistry;
     }
 
-    function isValid(address _wallet, address _spender, address _to, bytes calldata _data) external view override returns (bool) {
+    function isValid(address _wallet, address /*_spender*/, address /*_to*/, bytes calldata _data) external view override returns (bool) {
         (bytes32 sig,, address destToken) = abi.decode(abi.encodePacked(bytes28(0), _data), (bytes32, address, address));
         (address beneficiary) = abi.decode(_data[228:], (address)); // skipping 4 + 7*32 = 228 bytes
         return sig == 
