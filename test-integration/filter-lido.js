7d53386a7046fda92d60a7daf41d68d2f776fa3a
elenadimitrova <elena@arenabg.com>
2021-03-12 08:47:37 +0200

    Fix test

--- test-integration/filter-lido.js
+++ test-integration/filter-lido.js
@@ -20,8 +20,8 @@ const LidoFilter = artifacts.require("LidoFilter");
 const ILido = artifacts.require("ILido");
 
 // UniswapV2
-const UniswapV2Factory = artifacts.require("UniswapV2Factory");
-const UniswapV2Router01 = artifacts.require("UniswapV2Router01");
+const UniswapV2Factory = artifacts.require("UniswapV2FactoryMock");
+const UniswapV2Router01 = artifacts.require("UniswapV2Router01Mock");
 const WETH = artifacts.require("WETH9");
 
 const utils = require("../utils/utilities.js");
