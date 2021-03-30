970a5ce1f6335a6a4e36335edbfb2e089018e03e
Julien Niset <julien.niset@gmail.com>
2021-03-30 12:26:31 +0200

    Fixed typo

--- contracts-test/TestSimpleOracle.sol
+++ contracts-test/TestSimpleOracle.sol
@@ -16,7 +16,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.6.12;
 
-import "../contracts/modules/common/simpleOracle.sol";
+import "../contracts/modules/common/SimpleOracle.sol";
 
 contract TestSimpleOracle is SimpleOracle {
 
