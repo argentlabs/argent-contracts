2e7f42816faad519024f48118336741218dc6276
elenadimitrova <elena@arenabg.com>
2021-04-13 11:53:51 +0300

    Add test for areTokensTradable

--- test/tokenRegistry.js
+++ test/tokenRegistry.js
@@ -14,11 +14,14 @@ contract("TokenRegistry", (accounts) => {
   const manager = accounts[1];
 
   let tokenAddress;
+  let tokenAddress2;
   let tokenRegistry;
 
   before(async () => {
     const token = await ERC20.new([], 1, 18);
     tokenAddress = token.address;
+    const token2 = await ERC20.new([], 1, 18);
+    tokenAddress2 = token2.address;
   });
 
   beforeEach(async () => {
@@ -38,6 +41,7 @@ contract("TokenRegistry", (accounts) => {
       tradable = await tokenRegistry.isTokenTradable(tokenAddress);
       assert.isTrue(tradable);
     });
+
     it("lets managers set tradable to false only", async () => {
       await tokenRegistry.setTradableForTokenList([tokenAddress], [true], { from: owner });
       await tokenRegistry.setTradableForTokenList([tokenAddress], [false], { from: manager });
@@ -45,10 +49,23 @@ contract("TokenRegistry", (accounts) => {
       assert.isFalse(tradable);
       await truffleAssert.reverts(tokenRegistry.setTradableForTokenList([tokenAddress], [true], { from: manager }), "TR: Unauthorised");
     });
+
     it("does not let managers change tradable with invalid array lengths", async () => {
       await truffleAssert.reverts(
         tokenRegistry.setTradableForTokenList([tokenAddress], [false, false], { from: manager }),
         "TR: Array length mismatch");
+    });
+  });
+
+  describe("Reading tradable status", () => {
+    it("lets managers read tradable information on multiple tokens", async () => {
+      await tokenRegistry.setTradableForTokenList([tokenAddress], [true], { from: owner });
+      let tradable = await tokenRegistry.areTokensTradable([tokenAddress, tokenAddress2]);
+      assert.isFalse(tradable);
+
+      await tokenRegistry.setTradableForTokenList([tokenAddress2], [true], { from: owner });
+      tradable = await tokenRegistry.areTokensTradable([tokenAddress, tokenAddress2]);
+      assert.isTrue(tradable);
     });
   });
 });
