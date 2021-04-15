8c903b8cc07508f08d6c60f0ad34535a9caa6a00
elenadimitrova <elena@arenabg.com>
2021-04-15 18:39:47 +0300

    Use .call value syntax in lib contracts instead of .transfer

--- lib_0.5/maker/WETH9.sol
+++ lib_0.5/maker/WETH9.sol
@@ -38,7 +38,7 @@ contract WETH9 {
     function withdraw(uint wad) public {
         require(balanceOf[msg.sender] >= wad);
         balanceOf[msg.sender] -= wad;
-        msg.sender.transfer(wad);
+        msg.sender.call.value(wad)("");
         emit Withdrawal(msg.sender, wad);
     }
 
