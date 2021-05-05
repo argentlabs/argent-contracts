a61465f55c0758279fc5af002243b8639f53b36a
Olivier van den Biggelaar <ovdbigge@gmail.com>
2021-05-05 19:50:57 +0100

    Add filters for Gro

---
+++ lib_0.7/gro/DepositHandlerMock.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.6.0 <0.8.0;
+
+contract DepositHandlerMock {
+
+    function referral(address referee) external view returns (address) {}
+    
+    function depositGvt(
+        uint256[] calldata inAmounts,
+        uint256 minAmount,
+        address referral
+    ) external {}
+
+    function depositPwrd(
+        uint256[] calldata inAmounts,
+        uint256 minAmount,
+        address referral
+    ) external {}
+}
