fd0a626813955290822980ceed76a89b355709e1
Axel Delamarre <1091900+axeldelamarre@users.noreply.github.com>
2021-07-27 13:34:53 +0200

    Test for deposit/withdraw in Balancer pool

--- test-integration/filter-balancer.js
+++ test-integration/filter-balancer.js
@@ -5,6 +5,8 @@ const ArgentContext = require("../utils/argent-context.js");
 const BPool = artifacts.require("BPool");
 const BalancerFilter = artifacts.require("BalancerFilter");
 
+const amount = web3.utils.toWei("1");
+
 contract("Balancer Filter", (accounts) => {
   let argent, wallet;
   let pool;
@@ -19,20 +21,25 @@ contract("Balancer Filter", (accounts) => {
   });
 
   beforeEach(async () => {
-    wallet = await argent.createFundedWallet();
+    wallet = await argent.createFundedWallet({ WETH: "1" });
   });
 
+  const deposit = async () => argent.multiCall(wallet, [
+    [argent.WETH, "approve", [pool.address, amount]],
+    [pool, "joinswapExternAmountIn", [argent.WETH.address, amount, 1]]
+  ]);
+
   it("should allow deposits", async () => {
-    const amount = web3.utils.toWei("10");
-    const { success, error } = await argent.multiCall(wallet, [
-      // todo
-    ]);
+    const { success, error } = await deposit();
     assert.isTrue(success, `deposit failed: "${error}"`);
   });
 
   it("should allow withdrawals", async () => {
+    await deposit();
+    const bpt = await pool.balanceOf(wallet.address);
     const { success, error } = await argent.multiCall(wallet, [
-      // todo
+      [pool, "approve", [pool.address, bpt.toString()]],
+      [pool, "exitswapExternAmountOut", [argent.WETH.address, web3.utils.toWei("0.1"), bpt.toString()]]
     ]);
     assert.isTrue(success, `withdrawal failed: "${error}"`);
   });
