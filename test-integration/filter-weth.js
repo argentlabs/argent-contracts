faf050973a6e98e4a6c37eae0bd9e8490c52a508
Axel Delamarre <1091900+axeldelamarre@users.noreply.github.com>
2021-08-18 11:09:43 +0200

    Improve Yearn v2 tests

--- test-integration/filter-weth.js
+++ test-integration/filter-weth.js
@@ -21,34 +21,32 @@ contract("WETH Filter", (accounts) => {
     wallet = await argent.createFundedWallet();
   });
 
-  describe("WETH deposit and withdrawal", () => {
-    it("should allow deposit", async () => {
-      const { success, error } = await argent.multiCall(wallet, [
-        [argent.WETH, "deposit", [], 100]
-      ]);
-      assert.isTrue(success, `deposit failed: "${error}"`);
+  it("should allow deposit", async () => {
+    const { success, error } = await argent.multiCall(wallet, [
+      [argent.WETH, "deposit", [], 100]
+    ]);
+    assert.isTrue(success, `deposit failed: "${error}"`);
 
-      const walletBalance = await argent.WETH.balanceOf(wallet.address);
-      expect(walletBalance.toString()).to.equal("100");
-    });
+    const walletBalance = await argent.WETH.balanceOf(wallet.address);
+    expect(walletBalance.toString()).to.equal("100");
+  });
 
-    it("should allow deposit using fallback", async () => {
-      const { success, error } = await argent.multiCall(wallet, [utils.encodeTransaction(argent.WETH.address, 100, "0x")]);
-      assert.isTrue(success, `deposit failed: "${error}"`);
+  it("should allow deposit using fallback", async () => {
+    const { success, error } = await argent.multiCall(wallet, [utils.encodeTransaction(argent.WETH.address, 100, "0x")]);
+    assert.isTrue(success, `deposit failed: "${error}"`);
 
-      const walletBalance = await argent.WETH.balanceOf(wallet.address);
-      expect(walletBalance.toString()).to.equal("100");
-    });
+    const walletBalance = await argent.WETH.balanceOf(wallet.address);
+    expect(walletBalance.toString()).to.equal("100");
+  });
 
-    it("should allow withdrawals", async () => {
-      const { success, error } = await argent.multiCall(wallet, [
-        [argent.WETH, "deposit", [], 100],
-        [argent.WETH, "withdraw", ["75"]],
-      ]);
-      assert.isTrue(success, `deposit/withdrawal failed: "${error}"`);
+  it("should allow withdrawals", async () => {
+    const { success, error } = await argent.multiCall(wallet, [
+      [argent.WETH, "deposit", [], 100],
+      [argent.WETH, "withdraw", ["75"]],
+    ]);
+    assert.isTrue(success, `deposit/withdrawal failed: "${error}"`);
 
-      const walletBalance = await argent.WETH.balanceOf(wallet.address);
-      expect(walletBalance.toString()).to.equal("25");
-    });
+    const walletBalance = await argent.WETH.balanceOf(wallet.address);
+    expect(walletBalance.toString()).to.equal("25");
   });
 });
--- test-integration/filter-yearn-v2.js
+++ test-integration/filter-yearn-v2.js
@@ -6,85 +6,135 @@ const utils = require("../utils/utilities.js");
 
 const Vault = artifacts.require("yVaultV2Mock");
 const YearnV2Filter = artifacts.require("YearnV2Filter");
+const WethFilter = artifacts.require("WethFilter");
 const ERC20 = artifacts.require("TestERC20");
 
 const amount = web3.utils.toWei("1");
+const ethAmount = web3.utils.toWei("0.01");
 
 contract("Yearn V2 Filter", (accounts) => {
   let argent;
   let wallet;
-  let vault;
-  let yvDai;
+
+  let daiVault;
+  let wethVault;
+  let yvDAI;
+  let yvWETH;
 
   before(async () => {
     argent = await deployArgent(accounts);
 
-    vault = await Vault.at("0x19D3364A399d251E894aC732651be8B0E4e85001");
-    yvDai = await ERC20.at(vault.address);
+    // TODO: use ABI unstead of mock to avoid having both variables
+    daiVault = await Vault.at("0x19D3364A399d251E894aC732651be8B0E4e85001");
+    wethVault = await Vault.at("0xa9fE4601811213c340e850ea305481afF02f5b28");
 
-    const filter = await YearnV2Filter.new();
-    await argent.dappRegistry.addDapp(0, vault.address, filter.address);
+    yvDAI = await ERC20.at(daiVault.address);
+    yvWETH = await ERC20.at(wethVault.address);
+
+    const yearnFilter = await YearnV2Filter.new();
+    const wethFilter = await WethFilter.new();
+
+    await argent.dappRegistry.addDapp(0, daiVault.address, yearnFilter.address);
+    await argent.dappRegistry.addDapp(0, wethVault.address, yearnFilter.address);
+    await argent.dappRegistry.addDapp(0, argent.WETH.address, wethFilter.address);
   });
 
-  beforeEach(async () => {
-    wallet = await argent.createFundedWallet({ DAI: amount });
+  describe("DAI vault", () => {
+    beforeEach(async () => {
+      wallet = await argent.createFundedWallet({ DAI: amount });
+    });
+
+    const deposit0 = async () => argent.multiCall(wallet, [
+      [argent.DAI, "approve", [daiVault.address, amount]],
+      [daiVault, "deposit", []]
+    ]);
+
+    const deposit1 = async () => argent.multiCall(wallet, [
+      [argent.DAI, "approve", [daiVault.address, amount]],
+      [daiVault, "deposit", [amount]]
+    ]);
+
+    it("should allow deposits (0 params)", async () => {
+      const { success, error } = await utils.checkBalances(wallet, argent.DAI, yvDAI, deposit0);
+      assert.isTrue(success, `deposit failed: "${error}"`);
+    });
+
+    it("should allow deposits (1 param)", async () => {
+      const { success, error } = await utils.checkBalances(wallet, argent.DAI, yvDAI, deposit1);
+      assert.isTrue(success, `deposit failed: "${error}"`);
+    });
+
+    it("should allow withdrawals (0 params)", async () => {
+      await deposit0();
+
+      const { success, error } = await utils.checkBalances(wallet, yvDAI, argent.DAI, () => (
+        argent.multiCall(wallet, [[daiVault, "withdraw", []]])
+      ));
+
+      assert.isTrue(success, `withdrawal failed: "${error}"`);
+    });
+
+    it("should allow withdrawals (1 param)", async () => {
+      await deposit1();
+
+      const { success, error } = await utils.checkBalances(wallet, yvDAI, argent.DAI, () => (
+        argent.multiCall(wallet, [[daiVault, "withdraw", [1]]])
+      ));
+
+      assert.isTrue(success, `withdrawal failed: "${error}"`);
+    });
+
+    it("should not allow direct transfers to pool", async () => {
+      const { success, error } = await argent.multiCall(wallet, [[argent.DAI, "transfer", [daiVault.address, amount]]]);
+      assert.isFalse(success, "transfer should have failed");
+      assert.equal(error, "TM: call not authorised");
+    });
+
+    it("should not allow calling unsupported method", async () => {
+      const { success, error } = await argent.multiCall(wallet, [[daiVault, "setManagementFee", [1]]]);
+      assert.isFalse(success, "setManagementFee() should have failed");
+      assert.equal(error, "TM: call not authorised");
+    });
+
+    it("should not allow sending ETH ", async () => {
+      const { success, error } = await argent.multiCall(wallet, [utils.encodeTransaction(daiVault.address, ethAmount, "0x")]);
+      assert.isFalse(success, "sending ETH should have failed");
+      assert.equal(error, "TM: call not authorised");
+    });
   });
 
-  const deposit0 = async () => argent.multiCall(wallet, [
-    [argent.DAI, "approve", [vault.address, amount]],
-    [vault, "deposit", []]
-  ]);
+  describe("WETH vault", () => {
+    beforeEach(async () => {
+      wallet = await argent.createFundedWallet({ WETH: ethAmount });
+    });
 
-  const deposit1 = async () => argent.multiCall(wallet, [
-    [argent.DAI, "approve", [vault.address, amount]],
-    [vault, "deposit", [amount]]
-  ]);
+    const depositETH0 = async () => argent.multiCall(wallet, [
+      [argent.WETH, "approve", [wethVault.address, ethAmount]],
+      [wethVault, "deposit", []]
+    ]);
 
-  it("should allow deposits (0 params)", async () => {
-    const { success, error } = await utils.checkBalances(wallet, argent.DAI, yvDai, deposit0);
-    assert.isTrue(success, `deposit failed: "${error}"`);
-  });
+    const depositETH1 = async () => argent.multiCall(wallet, [
+      [argent.WETH, "approve", [wethVault.address, ethAmount]],
+      [wethVault, "deposit", [ethAmount]]
+    ]);
 
-  it("should allow deposits (1 param)", async () => {
-    const { success, error } = await utils.checkBalances(wallet, argent.DAI, yvDai, deposit1);
-    assert.isTrue(success, `deposit failed: "${error}"`);
-  });
+    it("should allow ETH deposits (0 params)", async () => {
+      const { success, error } = await utils.checkBalances(wallet, argent.WETH, yvWETH, depositETH0);
+      assert.isTrue(success, `depositETH0 failed: "${error}"`);
+    });
 
-  it("should allow withdrawals (0 params)", async () => {
-    await deposit0();
+    it("should allow ETH deposits (1 params)", async () => {
+      const { success, error } = await utils.checkBalances(wallet, argent.WETH, yvWETH, depositETH1);
+      assert.isTrue(success, `depositETH1 failed: "${error}"`);
+    });
 
-    const { success, error } = await utils.checkBalances(wallet, yvDai, argent.DAI, () => (
-      argent.multiCall(wallet, [[vault, "withdraw", []]])
-    ));
+    it("should allow ETH withdrawals", async () => {
+      await depositETH1();
 
-    assert.isTrue(success, `withdrawal failed: "${error}"`);
-  });
-
-  it("should allow withdrawals (1 param)", async () => {
-    await deposit1();
-
-    const { success, error } = await utils.checkBalances(wallet, yvDai, argent.DAI, () => (
-      argent.multiCall(wallet, [[vault, "withdraw", [1]]])
-    ));
-
-    assert.isTrue(success, `withdrawal failed: "${error}"`);
-  });
-
-  it("should not allow direct transfers to pool", async () => {
-    const { success, error } = await argent.multiCall(wallet, [[argent.DAI, "transfer", [vault.address, amount]]]);
-    assert.isFalse(success, "transfer should have failed");
-    assert.equal(error, "TM: call not authorised");
-  });
-
-  it("should not allow calling unsupported method", async () => {
-    const { success, error } = await argent.multiCall(wallet, [[vault, "setManagementFee", [1]]]);
-    assert.isFalse(success, "setManagementFee() should have failed");
-    assert.equal(error, "TM: call not authorised");
-  });
-
-  it("should not allow sending ETH ", async () => {
-    const { success, error } = await argent.multiCall(wallet, [utils.encodeTransaction(vault.address, web3.utils.toWei("0.01"), "0x")]);
-    assert.isFalse(success, "sending ETH should have failed");
-    assert.equal(error, "TM: call not authorised");
+      const { success, error } = await utils.checkBalances(wallet, yvWETH, argent.WETH, () => argent.multiCall(wallet, [
+        [wethVault, "withdraw", []],
+      ]));
+      assert.isTrue(success, `withdrawETH failed: "${error}"`);
+    });
   });
 });
