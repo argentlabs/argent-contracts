48dd75cbde99c5978959e6f7983cfedff4f277f5
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-03-17 14:19:36 +0100

    test(gro): change controller owner

--- scripts/start_mainnet_fork.sh
+++ scripts/start_mainnet_fork.sh
@@ -21,6 +21,6 @@ node_modules/.bin/ganache-cli \
     --unlock "0xe982615d461dd5cd06575bbea87624fda4e3de17" `# USDC minter` \
     --unlock "0xc6cde7c39eb2f0f0095f41570af89efc2c1ea828" `# USDT minter` \
     --unlock "0x6c5024cd4f8a59110119c56f8933403a539555eb" `# sUSD whale (Aave)` \
-    --unlock "0xdc954086cf07f3889f186118395bad186179ac77" `# Gro Controller` \
+    --unlock "0x359f4fe841f246a095a82cb26f5819e10a91fe0d" `# Gro Controller owner` \
     --unlock "0x36fedc70fec3b77caaf50e6c524fd7e5dfbd629a" `# Paraswap owner` \
     --unlock "0xa5c603e1C27a96171487aea0649b01c56248d2e8" `# Argent ENS owner` \
